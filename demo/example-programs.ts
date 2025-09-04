5bfee8dc1b81cb01817e631f44d6acc51c9260aa
Sam Cohen <sbcohen2000@gmail.com>
2025-09-04 13:43:48 -0500

    Demo: Add list comprehension example program

--- demo/example-programs.ts
+++ demo/example-programs.ts
@@ -27,5 +27,30 @@ main =
   fill: rgba(100 150 200 0.3);
   border: 1.1 2 black;
   padding: 4;
+}`,
+  "List Comprehension": `
+[pairs]@nm =
+  [\\[ [([i]@nm, [j]@nm)]@expr
+     [for [i]@nm in [range([0]@nm, [10]@nm)]@expr
+     [for [j]@nm in [range([0]@nm, [10]@nm)]@expr
+     [if [i]@nm != [j]@nm]@stmt]@stmt]@stmt \\]]@expr
+
+@nm {
+  fill: #FAFA37;
+  border: 0 2;
+}
+
+@expr {
+  padding: 2;
+  fill: #FA9D5A;
+  border: 0.7 1 #D27D46;
+  border: 0.7 1 -0.7 #FFCBA4 top right;
+}
+
+@stmt {
+  padding: 2;
+  fill: gainsboro;
+  border: 0.7 1 gray;
+  border: 0.7 1 -0.7 white top right;
 }`
 };
